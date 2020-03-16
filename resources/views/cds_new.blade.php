55f6187beed12040235d18ce41ba5d06b9b34d5c
Andrea Bottini <andrea.bottini22@gmail.com>
2020-03-16 18:56:58 +0100

    creati partials, utilizzati section e yields. ok! Installato bootstrap

--- resources/views/cds_new.blade.php
+++ resources/views/cds_new.blade.php
@@ -1,38 +1,20 @@
 {{-- {{dd($cds)}} --}}
+@extends('layouts.app')
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
-    <title>Php Dischi</title>
-</head>
-
-<body>
-    <div class="container">
-        <header>
-            <img src="http://pluspng.com/img-png/spotify-logo-png-open-2000.png" alt="">
-        </header>
-
+ @section('header')
+    <img src="http://pluspng.com/img-png/spotify-logo-png-open-2000.png" alt="">
+     
+ @endsection   
+           
+@section('content')
         <div class="cds-container">
             @foreach ($cds as $cd)
-            <div class="cd">
-            <img src="{{$cd['imgpath']}}" alt="">
-            <h3>{{$cd['album']}}</h3>
-            <small>{{$cd['artist']}}</small>
-            <strong>{{$cd['year']}}</strong>
-                
-              
-            </div>
-                
-            @endforeach
-            
-            
+                <div class="cd">
+                    <img src="{{$cd['imgpath']}}" alt="">
+                    <h3>{{$cd['album']}}</h3>
+                    <small>{{$cd['artist']}}</small>
+                <strong>{{$cd['year']}}</strong>  
+                </div>    
+            @endforeach   
         </div>
-    </div>
-</body>
-
-</html>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+@include('partials._header')
+
+<div class="container">
+    <header>
+        @yield('header')
+    </header>
+    
+    <main>
+        @yield('content')
+
+    </main>
+</div>
+
+@include('partials._footer')
